c3e9fc442c9ef1eb6ad5ae328869177f00bbfae8
Sudheendra-RD <sudheendrard2@gmail.com>
2025-08-15 20:42:42 +0530

    [VERCEL]:removed Mangum handler

--- api/app.py
+++ api/app.py
@@ -186,7 +186,7 @@ def get_sunrise_sunset_time(rise_or_set, date):
     else:
         return s['sunset'].strftime('%I:%M:%S %p')
 
-# handler = Mangum(app)
+handler = Mangum(app)
 
 # if __name__ == '__main__':
 #     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
